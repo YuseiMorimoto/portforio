7447438dfe20b9fd8e8ca1ef983879c71a8e4493
YuseiMorimoto <Yusei0509@icloud.com>
2024-04-18 17:51:21 +0900

    ✨ business部分の作成

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6
+      bg-blue-gradient font-poppins
+      font-medium text-[18px] text-black
+      outline-none ${styles}`}
+    >
+      GetStarted
+    </button>
+  );
 };
 
 export default Button;
